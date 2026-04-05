1e9f07dfe762049c3ae2db11ed17978ff26ebb85
aanchalg1007 <107946012+aanchalg1007@users.noreply.github.com>
2026-04-06 01:46:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(fs.readFileSync('index.html'));
+}).listen(3000);
+
+console.log('Server running on port 3000');
